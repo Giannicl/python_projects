e62f2359d84f30a4822f4001f90b8bbfbf6f0a94
gzoldyck <glieuw-a@codam.nl>
2026-01-11 21:11:45 +0100

    4-1

---
+++ python_module04/ex0/ft_ancient_text.py
@@ -0,0 +1,17 @@
+def main():
+    """ This function opens, reads and closes a text file"""
+    try:
+        print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===")
+        filename = "ancient_fragment.txt"
+        print(f"Accessing Storage Vault: {filename}")
+        print("Connection established...")
+        print("\nRECOVERED DATA:")
+        file = open(filename,'r')
+        content = file.read()
+        print(content)
+        file.close()
+        print("\nData recovery complete. Storage unit disconnected.")
+    except FileNotFoundError:
+         print("ERROR: Storage vault not found. Run data generator first.")
+
+main()
---
+++ python_module04/ex1/ft_archive_creation.py
@@ -0,0 +1,22 @@
+def main():
+    print("=== CYBER ARCHIVES - PRESERVATION SYSTEM ===")
+    print()
+    
+    filename = "new_discovery.txt"
+    print(f"Initializing new storage unit: {filename}")
+    print("Storage unit created successfully...")
+    print("\nInscribing preservation data...")
+    file = open(filename, "w")
+    file.write("{[}ENTRY 001{]} New quantum algorithm discovered\n")
+    file.write("{[}ENTRY 002{]} Efficiency increased by 347%\n")
+    file.write("{[}ENTRY 003{]} Archived by Data Archivist trainee\n")
+    file.close()
+    file = open(filename, "r")
+    content = file.read()
+    print(content)
+    file.close()
+    print("Data inscription complete. Storage unit sealed.")
+    print(f"Archive '{filename}' ready for long-term preservation.")
+    
+
+main()
