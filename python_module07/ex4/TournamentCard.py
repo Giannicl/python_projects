e1187c328e60bc0eac591c81f6bf37dd9da107e6
Gianni Lieuw-A-Soe <glieuw-a@f1r2s15.codam.nl>
2026-02-02 11:59:38 +0100

    7-4 done

--- python_module07/ex4/TournamentCard.py
+++ python_module07/ex4/TournamentCard.py
@@ -2,32 +2,41 @@ from ex0.Card import Card
 from ex2.Combatable import Combatable
 from ex4.Rankable import Rankable
 from typing import Dict, List
-    
+
 
 class TournamentCard(Card, Combatable, Rankable):
-    def __init__(self, name: str, cost: int, rarity: str, 
-                 attack: int, health: int, combat_type: str):
+    def __init__(
+        self,
+        name: str,
+        cost: int,
+        rarity: str,
+        attack: int,
+        health: int,
+        combat_type: str,
+    ):
         super().__init__(name, cost, rarity)
         self.attack_power = attack
         self.health = health
         self.defence = self.health // 6
         self.wins = 0
         self.losses = 0
-        self.score = 0
+        self.rating = 1200
         self.combat_type = combat_type
 
     def play(self, game_state: Dict) -> Dict:
         return {
-                "card_played": self.name,
-                "mana_used": self.cost,
-                "effect": "Tournament card summoned to the battleield",
-               }
+            "card_played": self.name,
+            "mana_used": self.cost,
+            "effect": "Tournament card summoned to the battleield",
+        }
+
     def attack(self, target) -> Dict:
         return {
-                "attacker": self.name,
-                "target": target,
-                "damage": self.attack_power,
-                }
+            "attacker": self.name,
+            "target": target,
+            "damage": self.attack_power,
+        }
+
     def defend(self, incoming_damage: int) -> Dict:
         remainder_damage = incoming_damage - self.defence
         self.health = self.health - remainder_damage
@@ -38,6 +47,7 @@ class TournamentCard(Card, Combatable, Rankable):
             "damage_blocked": incoming_damage - remainder_damage,
             "still_alive": alive,
         }
+
     def get_combat_stats(self):
         return {
             "attack_power": self.attack_power,
@@ -45,19 +55,26 @@ class TournamentCard(Card, Combatable, Rankable):
             "combat_type": self.combat_type,
             "defence": self.defence,
         }
+
     def calculate_rating(self) -> int:
-        return self.score - (self.losses * 50) + (self.wins * 50)
-    def update_wins(self, wins: int)-> None:
+        return self.rating - (self.losses * 50) + (self.wins * 50)
+
+    def update_wins(self, wins: int) -> None:
         self.wins = wins
-    def update_losses(self, losses: int)-> None:
+
+    def update_losses(self, losses: int) -> None:
         self.losses = losses
-    def get_rank_info(self,)-> Dict:
-        return { 
+
+    def get_rank_info(
+        self,
+    ) -> Dict:
+        return {
             "name": self.name,
             "rating": self.calculate_rating(),
             "wins": self.wins,
             "losses": self.losses,
         }
+
     def get_tournament_stats(self) -> Dict:
         return {
             "name": self.name,
@@ -68,7 +85,5 @@ class TournamentCard(Card, Combatable, Rankable):
             "rating": self.calculate_rating(),
             "wins": self.wins,
             "losses": self.losses,
-            "record": f"{self.wins}-{self.losses}"
-            }
-
-
+            "record": f"{self.wins}-{self.losses}",
+        }
--- python_module07/ex4/TournamentPlatform.py
+++ python_module07/ex4/TournamentPlatform.py
@@ -1,24 +1,25 @@
 from ex4.TournamentCard import TournamentCard
 from typing import Dict, List
 
+
 class TournamentPlatform:
     def __init__(self):
         self.id = {}
         self.id_number = 000
         self.matches_played = 0
-        
-    def register_card(self, card: TournamentCard) -> str: 
+
+    def register_card(self, card: TournamentCard) -> str:
         card_type = ""
-        if "dragon" in card.name or "dragon" in card.name:
+        if "dragon" in card.name or "Dragon" in card.name:
             card_type = "dragon"
-        elif "wizard" in card.name or "wizard" in card.name:
+        elif "wizard" in card.name or "Wizard" in card.name:
             card_type = "wizard"
         else:
             card_type = "unknown_type"
         self.id_number = self.id_number + 1
         self.id[card_type + f"_{self.id_number}"] = card
         return card_type + f"_{self.id_number}"
-    
+
     def create_match(self, card1_id: str, card2_id: str) -> Dict:
         combatant1 = self.id[card1_id]
         combatant2 = self.id[card2_id]
@@ -40,39 +41,44 @@ class TournamentPlatform:
             loser = card2_id
             combatant2.losses = combatant2.losses + 1
         return {
-                "winner": winner,
-                "loser": loser,
-                "winner_rating": self.id[winner].calculate_rating(),
-                "loser_rating": self.id[loser].calculate_rating(),
+            "winner": winner,
+            "loser": loser,
+            "winner_rating": self.id[winner].calculate_rating(),
+            "loser_rating": self.id[loser].calculate_rating(),
         }
+
     def get_leaderboard(self) -> List:
         card_info = []
         for card_id in self.id:
             card = self.id[card_id]
-            card_info = card_info + [{
+            card_info = card_info + [
+                {
                     "id": card_id,
                     "name": card.name,
                     "rating": card.calculate_rating(),
                     "wins": card.wins,
                     "losses": card.losses,
-            }]
+                }
+            ]
         size = 0
         for card in card_info:
             size = size + 1
 
         index = 0
-        while index < size: 
+        while index < size:
             index2 = 0
-            while index2 < size - 1 - index: 
+            while index2 < size - 1 - index:
                 info = card_info[index]
-                info2 = card_info[index2 + 1] 
+                info2 = card_info[index2 + 1]
                 if info["rating"] < info2["rating"]:
-                    temp = card_info[index2] 
+                    temp = card_info[index2]
                     card_info[index2] = card_info[index2 + 1]
                     card_info[index2 + 1] = temp
-            index2 = index2 + 1
+
+                index2 = index2 + 1
             index = index + 1
         return card_info
+
     def generate_tournament_report(self) -> Dict:
         size = 0
         for card_info in self.id:
@@ -80,11 +86,11 @@ class TournamentPlatform:
         total_rating = 0
         for card_info in self.id:
             card = self.id[card_info]
-            total_rating = total_rating + card.calculate_rating() 
-        average_rating = total_rating / size if size > 0 else 0 
+            total_rating = total_rating + card.calculate_rating()
+        average_rating = total_rating / size if size > 0 else 0
         return {
             "total_cards": size,
-            "matches_played": self.total_matches_played,
+            "matches_played": self.matches_played,
             "average_rating": average_rating,
-            "platorm_status": 'active'
+            "platorm_status": "active",
         }
