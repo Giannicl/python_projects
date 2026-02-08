14288aefea7f0a1070d07e87f5bec98e5856051e
gzoldyck <glieuw-a@codam.nl>
2026-02-08 10:16:04 +0100

    10-4 no main

--- python_module10/ex4/decorator_mastery.py
+++ python_module10/ex4/decorator_mastery.py
@@ -16,21 +16,54 @@ def power_validator(min_power: int) -> callable:
     def decorator(func: callable) -> callable:
         @wraps(func)
         def wrapper(*args, **kwargs) -> Any:
-            power = args[0]
-            if power >= min_power:
-                return func(*args, **kwargs)
-            else:
-                return "Insufficient power for this spell"
+            try:
+                power = args[2]
+
+                if power >= min_power:
+                    return func(*args, **kwargs)
+                else:
+                    return "Insufficient power for this spell"
+            except Exception:
+                print("power is the third positional argument")
+                return "Invalid spell"
         return wrapper
     return decorator
 
 
 def retry_spell(max_attempts: int) -> callable:
-    pass
+    def decorator(func: callable) -> callable:
+        @wraps(func)
+        def wrapper(*args, **kwargs)-> Any:
+            attempt = 1
+            while attempt <= max_attempts:
+                try:
+                    result = func(*args, **kwargs)
+                    return result
+                except Exception:
+                    if attempt == max_attempts:
+                        return f"Spell casting failed after {max_attempts} attempts"
+                    else:
+                        attempt = attempt + 1
+                        print(f"Spell failed, retrying... (attempt {attempt}/{max_attempts})")
+        return wrapper
+    return decorator
 
 class MageGuild:
     @staticmethod
     def validate_mage_name(name: str) -> bool:
-        pass
+        size = 0  
+        for character in name:
+            size = size + 1
+            is_space = character == ' ' 
+            is_lower_case = 'a' <= character <= 'z' 
+            is_upper_case = 'A' <= character <= 'Z'
+            if not(is_space or is_lower_case or is_upper_case):
+                return False
+        if size < 3:
+            return False
+        return True
+    @power_validator(10)   
     def cast_spell(self, spell_name: str, power: int) -> str:
-        pass
+        if MageGuild.validate_mage_name(spell_name):
+            return f"Successfully cast {spell_name} with power {power}" 
+        return "Invalid spell name"
